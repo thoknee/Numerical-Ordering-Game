083fbb3d70f24406c4a9054fea0d1ed4d0d09227
thoknee <antonyp@bu.edu>
2023-08-21 11:37:08 -0700

    added the win/loss/play again features

--- script.js
+++ script.js
@@ -10,7 +10,11 @@ let total = 0;
 function getNum(){
 
     let val = Math.floor(Math.random() * 1001);
-    for(ii=0;ii<20;ii++)
+    for(ii=0;ii<20;ii++){
+        if(val == arr[ii]){
+            getNum();
+        }
+    }
 
     curVal=val;
     return val;
@@ -20,7 +24,7 @@ function open(val){
 
     for(let i = 0; i < 20; i++){
         if(arr[i]==null){
-            document.getElementById(`num` + `${i+1}`).classList.add("open");
+                document.getElementById(`num` + `${i+1}`).classList.add("open");
                 document.getElementById(`num` + `${i+1}`).classList.remove("closed");
                 document.getElementById(`num` + `${i+1}`).disabled = false;
         }
@@ -32,7 +36,7 @@ function open(val){
         if(curVal > arr[i]){
 
             for(let j = 0; j < i; j++){
-                if(!(document.getElementById(`num` + `${j+1}`).classlist.contains("filled"))){
+                if(arr[j] == null){
                 document.getElementById(`num` + `${j+1}`).classList.add("closed");
                 document.getElementById(`num` + `${j+1}`).classList.remove("open");
                 document.getElementById(`num` + `${j+1}`).disabled = true;
@@ -52,7 +56,7 @@ function open(val){
 
                 for(let j = 19; j > i; j --){
                     
-                    if(!(document.getElementById(`num` + `${j+1}`).classList.contains("filled"))){
+                    if((arr[j] == null)){
                     document.getElementById(`num` + `${j+1}`).classList.add("closed");
                     document.getElementById(`num` + `${j+1}`).classList.remove("open");
                     document.getElementById(`num` + `${j+1}`).disabled = true;
@@ -71,6 +75,8 @@ function open(val){
 function BtnClick(ctrl,iR)
 {
     
+    
+    
     total++;
     ctrl.innerHTML=curVal;
     arr[iR]=curVal;
@@ -86,12 +92,22 @@ function BtnClick(ctrl,iR)
     if(total == 20){
         alert("you won!");
     }
+    let fail = 0;
+    for(let i =0; i <20; i ++){
+        if(document.getElementById(`num` + `${i+1}`).classList.contains("open")){
+            fail++;
+        }
+    }
+    if(fail ==0){
+       playAgainButton();
+    }
+    
 
 }
 
 function newNum(){
-
-    
+    document.querySelector(".playAgain").style.display = "none";
+    document.querySelector('.randomized-number').style.display = "block";
     
 
     let num = getNum();
@@ -102,6 +118,24 @@ function newNum(){
     document.querySelector(".playButton").style.display = "none";
   
 }
+
+function playAgainButton(){
+    document.querySelector(".playAgain").style.display = "block";
+}
+
+function playAgain(){
+    for(let i =0; i<20; i++){
+        arr[i] = null;
+        
+        
+        document.getElementById(`num` + `${i+1}`).innerHTML = "__";
+    }
+        open(0);
+        document.querySelector(".playButton").style.display = "block";
+        document.querySelector('.randomized-number').style.display = "none";
+        document.querySelector(".playAgain").style.display = "none";
+}
+
 
 
 
